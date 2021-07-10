0cc7755c3028830520febb66546e8d1aaaea2d96
Victor Huang <huangxd@yahoo.com>
2021-07-10 16:04:26 -0700

    2nd commit

--- init.sh
+++ init.sh
@@ -5,8 +5,8 @@ set e+
 echo "run it as \"source $0 \""
  sudo apt-get install -y --force-yes golint #- Linter for Go source code
 
-mkdir -p ~/src/golang/editor/bed
-cd  ~/src/golang/editor/bed
+#mkdir -p ~/src/golang/editor/bed
+#cd  ~/src/golang/editor/bed
 
 export GOPATH=$PWD
 
@@ -16,13 +16,16 @@ ls bin -lth
 cd   src/github.com/itchyny/bed/cmd
 git clone https://github.com/bayvictor/bed.git 
 cd bed 
+pwd
+ln -s $PWD ~/newbed 
 ./patch_it_after_checkout.sh
 #cd   src/github.com/itchyny/bed/cmd/bed
-go build
+
+go build  pkg/mod/github.com/itchyny/bed@v0.2.1/cmd/bed/main.go 
 ls 
 
-cp bed ~/sbin
+cp bin/bed ~/sbin
 which bed
-
+ls -lth  ~/sbin
 
 
